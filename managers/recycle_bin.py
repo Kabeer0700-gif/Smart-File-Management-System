571629e9e8d3cc838c700c011164c179a9391913
Kabeer0700-gif <kb3463217@gmail.com>
2026-06-17 20:53:31 +0500

    add recycle bin manager and its methods

---
+++ managers/recycle_bin.py
@@ -0,0 +1,106 @@
+from pathlib import Path
+import shutil
+import json
+class RecycleBinManager:
+    def __init__(self):
+        self.recycle_bin = Path('recycle_bin')
+        self.recycle_bin.mkdir(exist_ok=True)
+
+        self.metadata_file = Path('recycle_bin_metadata.json')
+
+        if not self.metadata_file.exists():
+
+            self.metadata_file.write_text(
+                "{}"
+            )
+
+    def move_to_recycle_bin(self, file):
+
+        source = Path(file.path)
+
+        destination = (
+            self.recycle_bin /
+            source.name
+        )
+
+        shutil.move(
+            str(source),
+            str(destination)
+        )
+
+        metadata = json.loads(
+            self.metadata_file.read_text()
+        )
+
+        metadata[source.name] = str(source)
+
+        self.metadata_file.write_text(
+            json.dumps(
+                metadata,
+                indent=4
+            )
+        )
+
+    def restore_file(self,filename):
+
+        metadata = json.loads(
+            self.metadata_file.read_text()
+        )
+
+        if filename not in metadata:
+
+            print(
+                "File not found"
+            )
+
+            return
+
+        original_path = Path(
+            metadata[filename]
+        )
+
+        source = (
+            self.recycle_bin /
+            filename
+        )
+
+        original_path.parent.mkdir(
+            parents=True,
+            exist_ok=True
+        )
+
+        shutil.move(
+            str(source),
+            str(original_path)
+        )
+
+        del metadata[filename]
+
+        self.metadata_file.write_text(
+            json.dumps(
+                metadata,
+                indent=4
+            )
+        )
+
+        print(
+            f"Restored: {filename}"
+        )
+
+    def delete_permanently(self,filename):
+
+        file_path = (
+            self.recycle_bin /
+            filename
+        )
+
+        if file_path.exists():
+
+            file_path.unlink()
+
+            print(
+                f"Deleted: {filename}"
+            )    
+
+    def list_files(self):
+        return list(self.recycle_bin.iterdir())
