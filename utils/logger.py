f0e602dbc204bf62719814a8d12c830df4886a6a
Kabeer0700-gif <kb3463217@gmail.com>
2026-06-18 20:27:31 +0500

    add log method for wrriting logs

--- utils/logger.py
+++ utils/logger.py
@@ -4,4 +4,26 @@ class Logger:
     def __init__(self):
         self.log_file = "app.log"
 
+    def log(self,level,message):
+        timestamp = (
+            datetime.now()
+            .strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
+        )
+
+        with open(
+            self.log_file,
+            "a",
+            encoding="utf-8"
+        ) as file:
+
+            file.write(
+                f"[{timestamp}] "
+                f"[{level}] "
+                f"{message}\n"
+            )
+
+
+logger = Logger()
     
