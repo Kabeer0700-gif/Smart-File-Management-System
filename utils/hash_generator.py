2234185982f24b6bbc83bf21323ed028dd6e94cb
Kabeer0700-gif <kb3463217@gmail.com>
2026-06-19 20:24:02 +0500

    add exception handling in hash generator

--- utils/hash_generator.py
+++ utils/hash_generator.py
@@ -4,13 +4,17 @@ class HashGenerator:
 
     @staticmethod
     def generate(filepath):
+        try:
+            sha256 = hashlib.sha256()
 
-        sha256 = hashlib.sha256()
+            with open(filepath, "rb") as file:
 
-        with open(filepath, "rb") as file:
+                while chunk := file.read(4096):
 
-            while chunk := file.read(4096):
-
-                sha256.update(chunk)
+                    sha256.update(chunk)
+        except Exception as e:
+            print(
+                f"Error Occured as {e}"
+            )
 
         return sha256.hexdigest()
