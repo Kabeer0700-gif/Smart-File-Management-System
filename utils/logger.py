c235576a8e4a2f1538ea891fa0bb97eddc553bd3
Kabeer0700-gif <kb3463217@gmail.com>
2026-06-19 20:12:09 +0500

    add exception handling in logger class

--- utils/logger.py
+++ utils/logger.py
@@ -5,25 +5,43 @@ class Logger:
         self.log_file = "app.log"
 
     def log(self,level,message):
-        timestamp = (
-            datetime.now()
-            .strftime(
-                "%Y-%m-%d %H:%M:%S"
-            )
-        )
-
-        with open(
-            self.log_file,
-            "a",
-            encoding="utf-8"
-        ) as file:
-
-            file.write(
-                f"[{timestamp}] "
-                f"[{level}] "
-                f"{message}\n"
+        try: 
+            timestamp = (
+                datetime.now()
+                .strftime(
+                    "%Y-%m-%d %H:%M:%S"
+                )
             )
 
+            with open(
+                self.log_file,
+                "a",
+                encoding="utf-8"
+            ) as file:
+
+                file.write(
+                    f"[{timestamp}] "
+                    f"[{level}] "
+                    f"{message}\n"
+                )
+        except PermissionError:
+
+            print(
+                "Logger Error: "
+                "Permission denied."
+            )
+
+        except OSError as e:
+
+            print(
+                f"Logger Error: {e}"
+            )
+
+        except Exception as e:
+
+            print(
+                f"Unexpected Logger Error: {e}"
+            )
 
 logger = Logger()
     
